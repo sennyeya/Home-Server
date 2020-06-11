43c9c6747a529c453e4cc5ace5f717c731779bd8
asennyey <asennyey@email.arizona.edu>
2020-06-10 21:56:27 -0700

    removed other api call

--- client/src/config.js
+++
@@ -1,7 +0,0 @@
-// Simple config file with server addresses.
-// Replace with your url for cross device usage. Otherwise use locahost.
-let myUrl =  "192.168.0.33"
-export default {
-    api: "http://"+myUrl+":4000/api/",
-    publicApi: 'http://'+myUrl+':4000/public/'
-}
